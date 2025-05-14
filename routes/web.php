c37821d51b528bd7b46bf5d0bac1b06569360ea5
ezqel <ezekiel3499@gmail.com>
2025-05-14 15:39:34 +0300

    Add font and  button styles

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return to_route('dashboard');
 })->name('home');
 
 Route::get('dashboard', function () {
