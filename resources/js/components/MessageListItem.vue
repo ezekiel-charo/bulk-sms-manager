adc171b3324ae9543535d9d1af88fcf7872b08e6
ezqel <ezekiel3499@gmail.com>
2025-07-04 21:23:54 +0300

    Refactor MessageListItem component: wrap content in a single Link for improved navigation and code clarity.

--- resources/js/components/MessageListItem.vue
+++ resources/js/components/MessageListItem.vue
@@ -7,28 +7,30 @@ defineProps<{ message: Message }>();
 </script>
 
 <template>
-    <div class="flex cursor-pointer items-center border-b border-gray-200 p-2 pe-6 hover:bg-gray-50 dark:hover:bg-slate-900">
-        <span class="me-5 text-6xl text-gray-600 dark:text-stone-300">
-            <MessageCircle />
-        </span>
+    <Link :href="`/messages/${message.id}/delivery-reports`">
+        <div class="flex cursor-pointer items-center border-b border-gray-200 p-2 pe-6 hover:bg-gray-50 dark:hover:bg-slate-900">
+            <span class="me-5 text-6xl text-gray-600 dark:text-stone-300">
+                <MessageCircle />
+            </span>
 
-        <div class="grow dark:text-stone-300">
-            <div class="mb-1 text-sm font-medium text-gray-500 dark:text-gray-400">{{ message.content }}</div>
-            <div class="text-xs font-medium text-gray-600">
-                <span class="me-3">
-                    Recipients:
-                    <span class="rounded bg-gray-100 p-1 text-yellow-700">{{ message.num_recipients }}</span>
-                </span>
-                <span>
-                    Delivered: <span class="text-primary rounded bg-gray-100 p-1">{{ message.num_delivered }}</span>
-                </span>
+            <div class="grow dark:text-stone-300">
+                <div class="mb-1 text-sm font-medium text-gray-500 dark:text-gray-400">{{ message.content }}</div>
+                <div class="text-xs font-medium text-gray-600">
+                    <span class="me-3">
+                        Recipients:
+                        <span class="rounded bg-gray-100 p-1 text-yellow-700">{{ message.num_recipients }}</span>
+                    </span>
+                    <span>
+                        Delivered: <span class="text-primary rounded bg-gray-100 p-1">{{ message.num_delivered }}</span>
+                    </span>
+                </div>
             </div>
-        </div>
 
-        <Link :href="`/messages/${message.id}/delivery-reports`">
-            <button class="btn text-primary">
-                <Eye :size="14" />
-            </button>
-        </Link>
-    </div>
+            <Link :href="`/messages/${message.id}/delivery-reports`">
+                <button class="btn text-primary">
+                    <Eye :size="14" />
+                </button>
+            </Link>
+        </div>
+    </Link>
 </template>
